2ea8bef7196ec834cbba02080695fa63d3e454a4
Nikita Bardatskii <bardanik@fit.cvut.cz>
2023-12-13 21:31:25 +0100

    some implementation

--- 02/sample.cpp
+++ 02/sample.cpp
@@ -1,4 +1,5 @@
 #ifndef __PROGTEST__
+
 #include <algorithm>
 #include <cassert>
 #include <cctype>
@@ -33,25 +34,86 @@ struct Grammar {
 };
 #endif
 
-std::vector<size_t> trace(const Grammar&, const Word&);
+
+constexpr int NO_RULE_FOUND = -1;
+using Cell = map<int, vector<int>>;
+using Table = vector<vector<Cell>>;
+using Rules = vector<pair<Symbol, vector<Symbol>>>;
+
+int find_rule_index(const Rules &rules, const vector<Symbol> &s) {
+    for (int rule_index = 0; rule_index < rules.size(); rule_index++) {
+        if (s == rules[rule_index].second) {
+            return rule_index;
+        }
+    }
+    return NO_RULE_FOUND;
+}
+
+Symbol index_to_symbol(int index, const Rules &rules) {
+    return rules[index].first;
+}
+
+void print_table(const Table &t, const Grammar &g, const Word &w) {
+    for (int row = 0; row < t.size(); row++) {
+        for (int column = 0; column < t.size(); column++) {
+            if (column == 0)
+                cout << "|";
+            cout << "{";
+            bool first_element = true;
+            for (const auto &element: t[row][column]) {
+                if (first_element) {
+                    first_element = false;
+                    cout << " " << index_to_symbol(element.first, g.m_Rules) << " ";
+                    continue;
+                }
+                cout << " " << index_to_symbol(element.first, g.m_Rules) << ", ";
+            }
+            cout << "}|";
+        }
+        cout << endl;
+    }
+}
+
+std::vector<size_t> trace(const Grammar &g, const Word &w) {
+//    initialize table
+    size_t n = w.size();
+    Table T = Table(n, vector<Cell>(n));
+
+    for (int j = 0; j < n; j++) {
+        for (int i = 0; i < n - j; i++) {
+            if (j == 0) { // first column
+                int index = find_rule_index(g.m_Rules, {w[i]});
+                T[i][j].insert({index, {}});
+            }
+
+        }
+    }
+    print_table(T, g, w);
+    return {};
+}
 
 #ifndef __PROGTEST__
-int main()
-{
+
+int main() {
+
+    assert(vector<int>({1, 2, 3}) == vector<int>({1, 2, 3}));
+    assert(vector<int>({1, 2, 3}) != vector<int>({2, 1, 3}));
+    assert(vector<int>({1, 2, 3}) != vector<int>({1, 2}));
+
     Grammar g0{
-        {'A', 'B', 'C', 'S'},
-        {'a', 'b'},
-        {
-            {'S', {'A', 'B'}},
-            {'S', {'B', 'C'}},
-            {'A', {'B', 'A'}},
-            {'A', {'a'}},
-            {'B', {'C', 'C'}},
-            {'B', {'b'}},
-            {'C', {'A', 'B'}},
-            {'C', {'a'}},
-        },
-        'S'};
+            {'A', 'B', 'C', 'S'},
+            {'a', 'b'},
+            {
+                    {'S', {'A', 'B'}},
+                    {'S', {'B', 'C'}},
+                    {'A', {'B', 'A'}},
+                    {'A', {'a'}},
+                    {'B', {'C', 'C'}},
+                    {'B', {'b'}},
+                    {'C', {'A', 'B'}},
+                    {'C', {'a'}},
+            },
+            'S'};
 
     assert(trace(g0, {'b', 'a', 'a', 'b', 'a'}) == std::vector<size_t>({0, 2, 5, 3, 4, 6, 3, 5, 7}));
     assert(trace(g0, {'b'}) == std::vector<size_t>({}));
@@ -63,16 +125,16 @@ int main()
     assert(trace(g0, {'c', 'a'}) == std::vector<size_t>({}));
 
     Grammar g1{
-        {'A', 'B'},
-        {'x', 'y'},
-        {
-            {'A', {}},
-            {'A', {'x'}},
-            {'B', {'x'}},
-            {'A', {'B', 'B'}},
-            {'B', {'B', 'B'}},
-        },
-        'A'};
+            {'A', 'B'},
+            {'x', 'y'},
+            {
+                    {'A', {}},
+                    {'A', {'x'}},
+                    {'B', {'x'}},
+                    {'A', {'B', 'B'}},
+                    {'B', {'B', 'B'}},
+            },
+            'A'};
 
     assert(trace(g1, {}) == std::vector<size_t>({0}));
     assert(trace(g1, {'x'}) == std::vector<size_t>({1}));
@@ -81,20 +143,23 @@ int main()
     assert(trace(g1, {'x', 'x', 'x', 'x'}) == std::vector<size_t>({3, 4, 4, 2, 2, 2, 2}));
     assert(trace(g1, {'x', 'x', 'x', 'x', 'x'}) == std::vector<size_t>({3, 4, 4, 4, 2, 2, 2, 2, 2}));
     assert(trace(g1, {'x', 'x', 'x', 'x', 'x', 'x'}) == std::vector<size_t>({3, 4, 4, 4, 4, 2, 2, 2, 2, 2, 2}));
-    assert(trace(g1, {'x', 'x', 'x', 'x', 'x', 'x', 'x'}) == std::vector<size_t>({3, 4, 4, 4, 4, 4, 2, 2, 2, 2, 2, 2, 2}));
-    assert(trace(g1, {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'}) == std::vector<size_t>({3, 4, 4, 4, 4, 4, 4, 2, 2, 2, 2, 2, 2, 2, 2}));
-    assert(trace(g1, {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'}) == std::vector<size_t>({3, 4, 4, 4, 4, 4, 4, 4, 2, 2, 2, 2, 2, 2, 2, 2, 2}));
+    assert(trace(g1, {'x', 'x', 'x', 'x', 'x', 'x', 'x'}) ==
+           std::vector<size_t>({3, 4, 4, 4, 4, 4, 2, 2, 2, 2, 2, 2, 2}));
+    assert(trace(g1, {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'}) ==
+           std::vector<size_t>({3, 4, 4, 4, 4, 4, 4, 2, 2, 2, 2, 2, 2, 2, 2}));
+    assert(trace(g1, {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'}) ==
+           std::vector<size_t>({3, 4, 4, 4, 4, 4, 4, 4, 2, 2, 2, 2, 2, 2, 2, 2, 2}));
 
     Grammar g2{
-        {'A', 'B'},
-        {'x', 'y'},
-        {
-            {'A', {'x'}},
-            {'B', {'x'}},
-            {'A', {'B', 'B'}},
-            {'B', {'B', 'B'}},
-        },
-        'B'};
+            {'A', 'B'},
+            {'x', 'y'},
+            {
+                    {'A', {'x'}},
+                    {'B', {'x'}},
+                    {'A', {'B', 'B'}},
+                    {'B', {'B', 'B'}},
+            },
+            'B'};
 
     assert(trace(g2, {}) == std::vector<size_t>({}));
     assert(trace(g2, {'x'}) == std::vector<size_t>({1}));
@@ -102,29 +167,31 @@ int main()
     assert(trace(g2, {'x', 'x', 'x'}) == std::vector<size_t>({3, 3, 1, 1, 1}));
 
     Grammar g3{
-        {'A', 'B', 'C', 'D', 'E', 'S'},
-        {'a', 'b'},
-        {
-            {'S', {'A', 'B'}},
-            {'S', {'S', 'S'}},
-            {'S', {'a'}},
-            {'A', {'B', 'S'}},
-            {'A', {'C', 'D'}},
-            {'A', {'b'}},
-            {'B', {'D', 'D'}},
-            {'B', {'b'}},
-            {'C', {'D', 'E'}},
-            {'C', {'b'}},
-            {'C', {'a'}},
-            {'D', {'a'}},
-            {'E', {'S', 'S'}},
-        },
-        'S'};
+            {'A', 'B', 'C', 'D', 'E', 'S'},
+            {'a', 'b'},
+            {
+                    {'S', {'A', 'B'}},
+                    {'S', {'S', 'S'}},
+                    {'S', {'a'}},
+                    {'A', {'B', 'S'}},
+                    {'A', {'C', 'D'}},
+                    {'A', {'b'}},
+                    {'B', {'D', 'D'}},
+                    {'B', {'b'}},
+                    {'C', {'D', 'E'}},
+                    {'C', {'b'}},
+                    {'C', {'a'}},
+                    {'D', {'a'}},
+                    {'E', {'S', 'S'}},
+            },
+            'S'};
 
     assert(trace(g3, {}) == std::vector<size_t>({}));
     assert(trace(g3, {'b'}) == std::vector<size_t>({}));
     assert(trace(g3, {'a', 'b', 'a', 'a', 'b'}) == std::vector<size_t>({1, 2, 0, 3, 7, 1, 2, 2, 7}));
-    assert(trace(g3, {'a', 'b', 'a', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'a'}) == std::vector<size_t>({1, 1, 0, 4, 8, 11, 12, 0, 5, 6, 11, 11, 0, 4, 9, 11, 7, 11, 7, 2, 2}));
+    assert(trace(g3, {'a', 'b', 'a', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'a'}) ==
+           std::vector<size_t>({1, 1, 0, 4, 8, 11, 12, 0, 5, 6, 11, 11, 0, 4, 9, 11, 7, 11, 7, 2, 2}));
 }
+
 #endif
 
